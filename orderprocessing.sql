fc614a5590aa5d241efad7b8d442e47cb2dbc2ff
paelir <219194633+paelir@users.noreply.github.com>
2026-03-25 08:37:22 +0100

    sql aangepast

--- orderprocessing.sql
+++ orderprocessing.sql
@@ -3,16 +3,19 @@ CREATE TABLE Leden (
     LidID INT PRIMARY KEY IDENTITY(1,1), 
     Naam NVARCHAR(50) NOT NULL,        
     Email NVARCHAR(50) NOT NULL,        
-    Adres NVARCHAR(MAX) NOT NULL,        
-    Status NVARCHAR(50) NOT NULL         -
+    Adres NVARCHAR(50) NOT NULL,        
+    Status NVARCHAR(50) NOT NULL,
+    NamePlate Bit NOT NULL,
+    Diner Bit NOT NULL,
+    Taxi Bit NOT NULL          -
 );
 
 -- 2. Tabel voor de Events
 
 CREATE TABLE Events (
     EventID INT PRIMARY KEY IDENTITY(1,1),
-    Naam NVARCHAR(255) NOT NULL,
-    LocatieAdres NVARCHAR(MAX) NOT NULL,  
+    Naam NVARCHAR(50) NOT NULL,
+    LocatieAdres NVARCHAR(50) NOT NULL,  
     EventDatum DATETIME NOT NULL,        
     Ticketprijs DECIMAL(10,2) NOT NULL             
 );
@@ -23,7 +26,7 @@ CREATE TABLE Orders (
     LidID INT NOT NULL,
     EventID INT NOT NULL,
     InschrijfDatum DATETIME2 DEFAULT GETDATE(),
-    Ticketprijs DECIMAL(10,2) NOT NULL,
+    Totaalprijs DECIMAL(10,2) NOT NULL,
     CONSTRAINT FK_Lid FOREIGN KEY (LidID) REFERENCES Leden(LidID),
     CONSTRAINT FK_Event FOREIGN KEY (EventID) REFERENCES Events(EventID)
 );
